0f2999fbc723d31681a35f415947910536f3920b
Eden Choi <eden.choi@vis-mobility.com>
2024-03-07 14:05:20 +0800

    fix file structure

---
+++ lib/src/spotlight_holder.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:tutorial_spotlight/src/enums.dart';
+import 'package:tutorial_spotlight/src/spotlight_controller.dart';
+import 'package:tutorial_spotlight/src/spotlight_item.dart';
+import 'package:tutorial_spotlight/src/spotlight_overlay.dart';
+import 'package:tutorial_spotlight/src/widget_position.dart';
+
+class SpotlightHolder extends StatelessWidget {
+  final SpotlightController controller;
+  final Widget child;
+
+  const SpotlightHolder({
+    Key? key,
+    required this.controller,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListenableBuilder(
+      listenable: controller,
+      builder: (context, _) {
+        if (controller.isActive) {
+          return Stack(
+            children: [
+              child,
+              SpotlightOverlay(
+                targetKey: controller.currentKey,
+                padding: controller.activeConfig?.padding,
+                borderRadius: controller.activeConfig?.borderRadius,
+                showSpotlight: controller.activeConfig?.showSpotlight ?? true,
+              ),
+              if (controller.activeConfig != null)
+                _buildTooltip(context, config: controller.activeConfig!),
+            ],
+          );
+        } else {
+          return child;
+        }
+      },
+    );
+  }
+
+  /// Builds the tooltip widget based on the current [SpotlightItemConfig].
+  Widget _buildTooltip(
+    BuildContext context, {
+    required SpotlightItemConfig config,
+  }) {
+    final tooltip = config.tooltip;
+    if (config.showSpotlight) {
+      final (offset, size) =
+          WidgetPosition.getOffsetAndSize(controller.currentKey);
+      double? left;
+      double? top;
+      double? right;
+      double? bottom;
+
+      final targetVerticalCenter = offset.dy + size.height / 2;
+      final screenVerticalCenter = MediaQuery.of(context).size.height / 2;
+      final isTop = config.tooltipVerticalPosition ==
+              SpotlightTooltipVerticalPosition.top ||
+          config.tooltipVerticalPosition ==
+                  SpotlightTooltipVerticalPosition.automatic &&
+              targetVerticalCenter > screenVerticalCenter;
+
+      final topPadding = config.padding.top;
+      final bottomPadding = config.padding.bottom;
+      if (isTop) {
+        bottom = MediaQuery.of(context).size.height -
+            offset.dy +
+            topPadding +
+            config.tooltipVerticalOffset;
+      } else {
+        top = offset.dy +
+            size.height +
+            bottomPadding +
+            config.tooltipVerticalOffset;
+      }
+
+      switch (config.tooltipHorizontalPosition) {
+        case SpotlightTooltipHorizontalPosition.alignLeft:
+          left = offset.dx - config.tooltipHorizontalOffset;
+        case SpotlightTooltipHorizontalPosition.alignRight:
+          right = MediaQuery.of(context).size.width -
+              size.width -
+              offset.dx -
+              config.tooltipHorizontalOffset;
+        case SpotlightTooltipHorizontalPosition.center:
+          left = config.tooltipHorizontalOffset;
+          right = left;
+      }
+
+      final pointTo = Offset(
+        offset.dx + size.width / 2,
+        isTop
+            ? offset.dy - topPadding
+            : offset.dy + size.height + bottomPadding,
+      );
+
+      final SpotlightTooltipPosition tooltipPosition = SpotlightTooltipPosition(
+        pointTo: pointTo,
+        left: left,
+        top: top,
+        right: right,
+        bottom: bottom,
+      );
+
+      return tooltip(controller, tooltipPosition);
+    } else {
+      return tooltip(controller, const SpotlightTooltipPosition());
+    }
+  }
+}
---
+++ lib/src/spotlight_overlay_painter.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class SpotlightOverlayPainter extends CustomPainter {
+  final Rect rect;
+  final EdgeInsets? padding;
+  final double? borderRadius;
+
+  SpotlightOverlayPainter({
+    required this.rect,
+    this.padding,
+    this.borderRadius,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final overlayPaint = Paint()..color = Colors.black.withOpacity(0.7);
+    final radius =
+        borderRadius != null ? Radius.circular(borderRadius!) : Radius.zero;
+    final edgeInsets = padding != null ? padding! : EdgeInsets.zero;
+
+    final paddedRect = Rect.fromLTWH(
+      rect.left - edgeInsets.left,
+      rect.top - edgeInsets.top,
+      rect.width + edgeInsets.horizontal,
+      rect.height + edgeInsets.vertical,
+    );
+
+    final rrect = RRect.fromRectAndRadius(paddedRect, radius);
+
+    final spotlightPaint = Paint()..blendMode = BlendMode.clear;
+
+    canvas.saveLayer(Offset.zero & size, Paint());
+    canvas.drawRect(Offset.zero & size, overlayPaint);
+    canvas.drawRRect(rrect, spotlightPaint);
+    canvas.restore();
+  }
+
+  @override
+  bool shouldRepaint(SpotlightOverlayPainter oldDelegate) {
+    return rect != oldDelegate.rect;
+  }
+}
