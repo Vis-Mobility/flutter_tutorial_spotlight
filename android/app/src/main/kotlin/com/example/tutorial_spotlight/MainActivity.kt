d15d8a3e1d5c77d33ea10b2cc4461d70fc866c2a
Eden Choi <eden.choi@vis-mobility.com>
2024-03-07 13:13:57 +0800

    add example

--- android/app/src/main/kotlin/com/example/tutorial_spotlight/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tutorial_spotlight
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ lib/tutorial_spotlight.dart
@@ -0,0 +1,7 @@
+export 'enums.dart';
+export 'spotlight_controller.dart';
+export 'spotlight_holder.dart';
+export 'spotlight_item.dart';
+export 'spotlight_overlay_painter.dart';
+export 'spotlight_overlay.dart';
+export 'widget_position.dart';
